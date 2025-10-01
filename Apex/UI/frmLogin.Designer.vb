6f4ae9bcae2752f614ebc15bef0b81e5ecdbf18b
Melanie <sixtocorbobarreto@gmail.com>
2025-10-01 15:40:12 -0300

    Updated

--- Apex/UI/frmLogin.Designer.vb
+++ Apex/UI/frmLogin.Designer.vb
@@ -27,78 +27,85 @@ Partial Class frmLogin
         Me.txtPassword = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCrearUsuario = New System.Windows.Forms.Button()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.btnCrearUsuario = New System.Windows.Forms.Button() ' <-- LÍNEA AGREGADA
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btnLogin
         '
-        Me.btnLogin.Location = New System.Drawing.Point(35, 230)
+        Me.btnLogin.Location = New System.Drawing.Point(52, 354)
+        Me.btnLogin.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnLogin.Name = "btnLogin"
-        Me.btnLogin.Size = New System.Drawing.Size(100, 23)
+        Me.btnLogin.Size = New System.Drawing.Size(150, 35)
         Me.btnLogin.TabIndex = 2
         Me.btnLogin.Text = "Ingresar"
         Me.btnLogin.UseVisualStyleBackColor = True
         '
         'txtUsuario
         '
-        Me.txtUsuario.Location = New System.Drawing.Point(101, 161)
+        Me.txtUsuario.Location = New System.Drawing.Point(152, 248)
+        Me.txtUsuario.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtUsuario.Name = "txtUsuario"
-        Me.txtUsuario.Size = New System.Drawing.Size(100, 20)
+        Me.txtUsuario.Size = New System.Drawing.Size(148, 26)
         Me.txtUsuario.TabIndex = 0
         '
         'txtPassword
         '
-        Me.txtPassword.Location = New System.Drawing.Point(101, 196)
+        Me.txtPassword.Location = New System.Drawing.Point(152, 302)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.Size = New System.Drawing.Size(148, 26)
         Me.txtPassword.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(40, 164)
+        Me.Label1.Location = New System.Drawing.Point(60, 252)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.Size = New System.Drawing.Size(64, 20)
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Usuario"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(22, 199)
+        Me.Label2.Location = New System.Drawing.Point(33, 306)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.Size = New System.Drawing.Size(92, 20)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Contraseña"
         '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.Apex.My.Resources.Resources.Police
-        Me.PictureBox1.Location = New System.Drawing.Point(92, 33)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(120, 101)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 5
-        Me.PictureBox1.TabStop = False
-        '
         'btnCrearUsuario
         '
-        Me.btnCrearUsuario.Location = New System.Drawing.Point(167, 230)
+        Me.btnCrearUsuario.Location = New System.Drawing.Point(250, 354)
+        Me.btnCrearUsuario.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.btnCrearUsuario.Name = "btnCrearUsuario"
-        Me.btnCrearUsuario.Size = New System.Drawing.Size(100, 23)
+        Me.btnCrearUsuario.Size = New System.Drawing.Size(150, 35)
         Me.btnCrearUsuario.TabIndex = 6
         Me.btnCrearUsuario.Text = "Crear Usuario"
         Me.btnCrearUsuario.UseVisualStyleBackColor = True
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Apex.My.Resources.Resources.Police
+        Me.PictureBox1.Location = New System.Drawing.Point(98, 14)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(302, 212)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
         'frmLogin
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(302, 287)
-        Me.Controls.Add(Me.btnCrearUsuario) ' <-- LÍNEA AGREGADA
+        Me.ClientSize = New System.Drawing.Size(453, 419)
+        Me.Controls.Add(Me.btnCrearUsuario)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -106,6 +113,7 @@ Partial Class frmLogin
         Me.Controls.Add(Me.txtUsuario)
         Me.Controls.Add(Me.btnLogin)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "frmLogin"
